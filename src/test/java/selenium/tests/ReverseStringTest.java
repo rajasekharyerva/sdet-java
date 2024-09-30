c26e4ae229ee142888a3a85bf3711e719b723f7e
Rajasekhar Reddy Yerva <rajasekharreddyyerva@Rajasekhars-MacBook-Pro.local>
2024-10-01 02:46:10 +0530

    sdd

---
+++ src/test/java/selenium/tests/ReverseStringTest.java
@@ -0,0 +1,25 @@
+package selenium.tests;
+
+public class ReverseStringTest {
+    public static void main(String args[]) {
+        String input = "interview";
+        System.out.println(reverseStringConcat(input));
+        System.out.println(reverseStringStringBuilder(input));
+    }
+
+    private static StringBuilder reverseStringStringBuilder(String input) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=input.length()-1; i>=0;i--){
+            sb.append(input.charAt(i));
+        }
+        return sb;
+    }
+
+    private static String reverseStringConcat(String input) {
+        String output="";
+        for(int i=input.length()-1; i>=0;i--){
+            output=output + input.charAt(i);
+        }
+        return output;
+    }
+}
